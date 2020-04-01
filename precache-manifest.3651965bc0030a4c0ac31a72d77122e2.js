608e5dc4a068e7138b21820b3871581fae38075c
Takuyashomura <takudai1411@yahoo.co.jp>
2020-04-01 23:11:22 +0900

    Updates

---
+++ precache-manifest.3651965bc0030a4c0ac31a72d77122e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecbd6004fc7ee9a44058f5c10593d575",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "4286397592fad053d1b8",
+    "url": "/my-app/static/js/2.0139c456.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-app/static/js/2.0139c456.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80dcc2e6d3ce67cb24fd",
+    "url": "/my-app/static/js/main.707979ed.chunk.js"
+  },
+  {
+    "revision": "97c2bf9888f87e4795bc",
+    "url": "/my-app/static/js/runtime-main.88d891b8.js"
+  }
+]);
